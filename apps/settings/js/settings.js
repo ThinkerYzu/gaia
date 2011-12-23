066aecde3818658eb1fb2b347eb4d1ded6e8478d
Sinker <thinker@codemud.net>
2011-12-23 23:21:57 +0800

    Reindent

--- apps/settings/js/settings.js
+++ apps/settings/js/settings.js
@@ -4,43 +4,44 @@
 'use strict';
 
 (function() {
-    DataCall = {
-	      init: function() {
-            var dcenable = document.getElementById("data-call-enable");
-            
-            if(dcenable.checked) {
-                this.makeDataCall();
-            }
-            dcenable.addEventListener("changed", this.onChange);
-	      },
+  DataCall = {
+	  init: function() {
+      var dcenable = document.getElementById("data-call-enable");
+      
+      if(dcenable.checked) {
+        this.makeDataCall();
+      }
+      dcenable.addEventListener("changed", this.onChange);
+	  },
 
-        onChange: function(evt) {
-            var dcenable = event.originalTarget;
+    onChange: function(evt) {
+      var dcenable = event.originalTarget;
 
-            if(dcenable.checked) {
-                this.makeDataCall();
-            } else {
-                this.deactivate();
-            }
-        },
+      if(dcenable.checked) {
+        this.makeDataCall();
+      } else {
+        this.deactivate();
+      }
+    },
 
-        makeDataCall: function() {
-            var cdma = 1;
-            var apn = "internet";
-            var user = "";
-            var passwd = "";
-            var chappap = 0;
-            var pdptype = "IP";
-            
-            window.navigator.connect(cdma, apn,
-                                     user, passwd, chappap, pdptype);
-            dump("connect");
-        },
+    makeDataCall: function() {
+      var cdma = 1;
+      var apn = "internet";
+      var user = "";
+      var passwd = "";
+      var chappap = 0;
+      var pdptype = "IP";
+      
+      window.navigator.connect(cdma, apn,
+                               user, passwd, chappap, pdptype);
+      dump("connect");
+    },
 
-        deactivate: function() {
-          window.navigator.deactivate("0001", "0");
-            dump("deactivate");
-        },
-    }
+    deactivate: function() {
+      window.navigator.deactivate("0001", "0");
+      dump("deactivate");
+    },
+  }
 })();
+
 
